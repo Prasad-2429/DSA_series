14f9fb60a018549ff78b804484abc0a948faae73
Prasad Jadhav <prasadjadhavpatil9@gmail.com>
2025-07-05 21:51:46 +0530

    hello

---
+++ halfpyramidpattern.java
@@ -0,0 +1,14 @@
+public class halfpyramidpattern {
+    public static void main (String args[]){
+        int n=4;
+        for(int line=1; line<=n; line++){
+            //numbers print
+            //i=line
+            //j=number
+            for (int number=1; number<=line; number++){
+                System.out.print(number);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ starpattern.java
@@ -0,0 +1,18 @@
+//1.lines(+)
+//for (int line=1; line<=line ; star ++){
+//2.number of times (i)
+//for(int star=1; star<=line;star++){
+//3.what a print?
+//Syso(*);}
+public class starpattern {
+    public static void main(String args []){
+        for (int line=1; line<=100; line++){
+    
+        //one line 
+        for (int star=1; star<= line; star++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+}

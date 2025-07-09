6f9481f59c1cf35e3b0003f3e9a4cf3418675bd3
Prasad Jadhav <prasadjadhavpatil9@gmail.com>
2025-07-09 20:12:03 +0530

    Hey

---
+++ sum.java
@@ -0,0 +1,24 @@
+ //Write a Java method to compute the sum of the digits in an integer.
+ //(Hint : Approach this question in the following way :
+// a. Take a variable sum = 0
+ //b. Find the last digit of the number
+ //c. Add it to the sum
+ //d. Repeat a & b until the number becomes 0 
+import java.util.Scanner;
+public class sum {
+    public static void main (String[]args){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("input an integer :");
+        int digits = sc.nextInt();
+        System.out.println (sumDigits(digits));
+    }
+public static int sumDigits(int n ){
+    int sumOfDigits = 0;
+    while (n>0){
+        int lastDigit = n%10 ;
+        sumOfDigits += lastDigit ;
+        n/= 10;
+    }
+    return sumOfDigits;
+}
+}

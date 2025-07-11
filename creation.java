bc428abb59eb3d84c1e183e4b0fa7df32c841ff9
Prasad Jadhav <prasadjadhavpatil9@gmail.com>
2025-07-11 23:47:29 +0530

    Hey

---
+++ creation.java
@@ -0,0 +1,8 @@
+//import java.util.Scanner;
+//public class creation 
+    //public static void main (String args[]){
+       // int marks[]=new int [50];
+       // int numbers[]= {1,2,3,};
+    //int morenumbers[]={4,5,6};
+       // String fruits[]={"apple,","mango", "orange"};
+

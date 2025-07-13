54cda463f9baebc18696a051f912802eff8c25f4
Prasad Jadhav <prasadjadhavpatil9@gmail.com>
2025-07-13 15:19:31 +0530

    All filess uploaded successfully

--- input.java
+++ input.java
@@ -5,9 +5,15 @@ public class input {
         Scanner sc = new Scanner (System.in);
         // int phy;
         // phy = sc.nextInt();
+        System.out.print("Give phy Mark  ");
         marks[0] = sc.nextInt (); // phy
+
+        System.out.print("Give Chem Mark ");
         marks [1] = sc.nextInt ();// chem
+
+        System.out.print("Give Math Mark ");
         marks[2 ] = sc.nextInt ();// math
+        
         System.out.println("phy:" + marks [0]);
         System.out.println("che:" + marks[1]);
         System.out.println("math:" + marks [2]);

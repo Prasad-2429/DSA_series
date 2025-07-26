593c28f77243d98cd70b96332df7079f92725bcc
Prasad Jadhav <prasadjadhavpatil9@gmail.com>
2025-07-26 22:59:58 +0530

    Hey

---
+++ ifelseexample.java
@@ -0,0 +1,11 @@
+public class ifelseexample {
+    public static void main (String args[]){
+        int num = 10;
+        if (num>0){
+            System.out.println("The number is positive");
+        }else{
+            System.out.println("The number is non positive");
+        }
+        }
+    }
+    

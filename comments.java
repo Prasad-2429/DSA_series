72607888dca12ade27160ae6c806024c0e76bc02
Prasad Jadhav <prasadjadhavpatil9@gmail.com>
2025-07-27 21:05:32 +0530

    Hey

---
+++ comments.java
@@ -0,0 +1,14 @@
+public class comments {
+    public static void main (String args[]){
+        // This is a single line comment.
+        /* 
+         * This is a multi line comment.
+         * It spans multiple lines.
+         */
+        /**
+         * This is a Javadoc comment.
+         * It provides documentations for the code.
+         */
+    }
+    
+}

a4ef69f13caed4de66dc40ff007e92908a6ba86b
Prasad Jadhav <prasadjadhavpatil9@gmail.com>
2025-07-28 16:04:45 +0530

    Hey

---
+++ constructor.java
@@ -0,0 +1,15 @@
+//student () = constructor 
+public class constructor {
+    public static void main (String args []){
+        Student s1 = new Student ("Prasad");
+        System.out.println (s1.name);
+    }
+}
+class Student {
+    String name ;
+    int roll ;
+
+    Student (String name ){
+        this.name = name ;
+    }
+}

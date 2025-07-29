ab0832cac5f51cbdf16a1269ea22916797736e25
Prasad Jadhav <prasadjadhavpatil9@gmail.com>
2025-07-29 13:15:28 +0530

    Inheritance

---
+++ inheritance.java
@@ -0,0 +1,24 @@
+public class inheritance {
+    public static void main (String args[]){
+        Fish shark = new Fish ();
+        shark.eat();
+    }
+}
+//Base class 
+class Animal {
+    String color;
+    void eat (){
+        System.out.println ("eats");
+    }
+    void breathe(){
+        System.out.println ("breathe");
+    
+    }
+}
+// Derived class 
+class Fish extends Animal {
+    int fins ;
+    void swim (){
+        System.out.println("");
+    }
+}

fe8eb8961283e60fc3cdc1f985800c28d292a9d0
Prasad Jadhav <prasadjadhavpatil9@gmail.com>
2025-07-30 13:30:48 +0530

    Superkeyword

---
+++ Superkeyword.java
@@ -0,0 +1,19 @@
+public class Superkeyword {
+    public static void main (String args[]){
+        Horse h = new Horse ();
+        System.out.println (h.color);
+
+    }
+}
+class Animal {
+    String color;
+    Animal (){
+        System.out.println ("animal constructor is called");
+    }
+}
+class Horse extends Animal {
+    Horse (){
+        super.color = "Red";
+        System.out.println ("horse constructor is called");
+    }
+}

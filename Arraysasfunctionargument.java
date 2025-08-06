803521ced20e96cf5ce675c4f2500cc9a2dc08e7
Prasad Jadhav <prasadjadhavpatil9@gmail.com>
2025-08-06 11:00:13 +0530

    Arrays

---
+++ Arraysasfunctionargument.java
@@ -0,0 +1,17 @@
+public class Arraysasfunctionargument { 
+    public static void update ( int marks []){ //update function take argument as array
+        for(int i =0;i<marks.length;i++){ //loop , i = array index
+            marks [i] = marks [i]+1;
+
+        }
+    }
+    public static void main (String args[]){
+        int marks[]={97,98,99};
+        update (marks);
+        //For print your marks 
+        for (int i =0;i<marks.length;i++){
+            System.out.print(marks[i] +" ");
+        }
+        System.out.println();
+    }
+}

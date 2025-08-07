9cfbe36e3fc1cb609a39f27640f4cc2ec72f6b7d
Prasad Jadhav <prasadjadhavpatil9@gmail.com>
2025-08-08 00:46:59 +0530

    Insertion Sort

--- Insertionsortt.java
+++ Insertionsortt.java
@@ -1,4 +1,4 @@
-public class selectionsort {
+public class Insertionsortt {
     public static void bubblesort(int arr[]){
         for (int turn =0 ; turn<arr.length-1;turn++){
             for (int j=0;j<arr.length-1-turn;j++){
@@ -33,22 +33,14 @@ public class selectionsort {
         }
     }
     public static void insertionSort(int arr[]){
-        for (int i=1; i<arr.length;i++){
-            int curr = arr [i];
-            int prev = i-1;
-            //finding out the correct pos to insert
-            while (prev >=0 && arr [prev]> curr){
-                arr[prev+1] = arr [prev];
-                prev--;
-            }
-            //insertion
-            arr[prev+1] = curr;
+    for (int i =1; i<arr.length;i++){
+    int curr = i;
+    int prev = i-1;
+    //finding out the correct pos to insert
+    while (prev>=0 &&arr[prev]>arr[curr]){
     
-        }
-    }
-    public static void main (String args[]){
-        int arr [] = {5,4,1,3,2};
-        insertionSort(arr);
-        printArr(arr);
-    }
-}
+
+
+
+
+

55391cce0cbd563294c697dd8491d660ffae5cbd
Prasad Jadhav <prasadjadhavpatil9@gmail.com>
2025-08-09 15:47:51 +0530

    2D Array

---
+++ array.java
@@ -0,0 +1,40 @@
+/* prohram to find Transpose of Matrix ?
+Transpose of a matrix is the process of swapping the rows to columns For a 2*3 matrix,
+Matrix
+a11 a12 a13
+a21 a22 a23
+
+Transposed Matrix
+a11  a21
+a12  a22
+a13  a23
+*/
+public class array {
+    public static void main (String args []){
+        int row = 2 , column = 3;
+        int [][]  matrix = {{2,3,7},{5,6,7}};
+
+        //Display original matrix 
+        printMatrix(matrix);
+
+        //Transpose the matrix
+        int [][] transpose = new int [column][row];
+        for (int i=0;i<row;i++){
+            for(int j =0;j<column;j++){
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+        //print the transposed matrix
+        printMatrix(transpose);
+    }
+    public static void printMatrix(int [][] matrix ){
+        System.out.println("The matrix is :");
+        for (int i=0;i<matrix.length;i++){
+            for (int j=0; j<matrix.length;j++){
+                System.out.print(matrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+    }
+}

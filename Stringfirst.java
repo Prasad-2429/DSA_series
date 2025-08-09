e7c5b681d7e070c227da9d22fcfd4455ec3328e5
Prasad Jadhav <prasadjadhavpatil9@gmail.com>
2025-08-09 16:55:35 +0530

    String

---
+++ Stringfirst.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Stringfirst {
+    public static void main (String args[]){
+        char arr [] = { 'a','b','c','d'};
+        String str = "abcd";
+        String str2 = new String ("xyz");
+        //Strings are IMMUTABLE
+
+        Scanner sc = new Scanner (System.in);
+        String name;
+        name = sc.next();
+        System.out.println(name);
+    }
+}
---
+++ Stringlength.java
@@ -0,0 +1,6 @@
+public class Stringlength {
+    public static void main (String args[]){
+        String fullName = " Prasad Jadhav";
+        System.out.println(fullName.length());
+    }
+}
